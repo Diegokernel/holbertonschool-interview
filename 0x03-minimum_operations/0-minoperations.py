1348f4f5c0b3cb95d4e6a5342325dc093cd167bc
diegokernel <777@holbertonschool.com>
2020-02-25 23:31:04 +0000

    check for the problem

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Find minimum number of operations to create string with COPY & PASTE"""
+
+
+def minOperations(n):
+    """a method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.'"""
+    operations = 0
+    if n <= 1:
+        return 0
+
+    for i in range(2, int(n ** 1/2 + 1)):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    if n > 1:
+        operations += n
+    return operations
