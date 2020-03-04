dc24c294840748d1fc74be82e3db9b5be83e2d8c
diegokernel <777@holbertonschool.com>
2020-03-04 02:26:59 +0000

    fix file

--- 0x07-sandpiles/0-sandpiles.c
+++ 0x07-sandpiles/0-sandpiles.c
@@ -32,8 +32,8 @@ int is_unstable(int grid[3][3])
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			if (grid[i][j] > 3)
-				return (0);
-	return (1);
+				return (1);
+	return (0);
 }
 
 /**
@@ -42,24 +42,27 @@ int is_unstable(int grid[3][3])
  * @row: row to tumble
  * Return: void
  */
-void check_piles(int grid[3][3], int overs[3][3])
+void check_piles(int grid1[3][3], int grid2[3][3])
 {
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-		for (j = 0; j < 3; j++)
-			if (overs[i][j] == 1)
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+		{
+			if (grid1[i][j] > 3)
 			{
-				grid[i][j] -= 4;
 				if (i > 0)
-					grid[i - 1][j] += 1;
-				if (i < 2)
-					grid[i + 1][j] += 1;
+					grid2[i - 1][j] += 1;
 				if (j > 0)
-					grid[i][j - 1] += 1;
+					grid2[i][j - 1] += 1;
+				if (i < 2)
+					grid2[i + 1][j] += 1;
 				if (j < 2)
-					grid[i][j + 1] += 1;
+					grid2[i][j + 1] += 1;
+				grid1[i][j] -= 4;
 			}
+		}
+	}
+	add_sandpiles(grid1, grid2);
 }
 
 /**
@@ -73,7 +76,10 @@ void add_sandpiles(int grid1[3][3], int grid2[3][3])
 
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
+		{
 			grid1[i][j] += grid2[i][j];
+			grid2[i][j] = 0;
+		}
 }
 
 /**
@@ -86,8 +92,9 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	add_sandpiles(grid1, grid2);
 	while (is_unstable(grid1))
 	{
+		printf("=\n");
 		print_grid(grid1);
-	        check_piles(grid1, grid2);
+		check_piles(grid1, grid2);
 	}
 
 }
