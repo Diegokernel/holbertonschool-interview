11cfae834c259346ae0ec4206a3f3089cd22cda0
diegokernel <777@holbertonschool.com>
2020-05-20 02:56:00 +0000

    d

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -32,8 +32,8 @@ int is_unstable(int grid[3][3])
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			if (grid[i][j] > 3)
-				return (1);
-	return (0);
+				return (0);
+	return (1);
 }
 
 /**
@@ -47,39 +47,39 @@ void add_sandpiles(int grid1[3][3], int grid2[3][3])
 
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
-		{
 			grid1[i][j] += grid2[i][j];
-			grid2[i][j] = 0;
-		}
 }
 
 /**
  * check_piles- tumble a sandpile grid square
- * @grid1: grid to tumble on
- * @grid2: row to tumble
+ * @grid: grid to tumble on
  * Return: void
  */
-void check_piles(int grid1[3][3], int grid2[3][3])
+void check_piles(int grid[3][3])
 {
-	for (int i = 0; i <= 2; i++)
+	int i, j;
+	int r_grid[3][3] = {
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0}};
+	for (i = 0; i < 3; i++)
 	{
-		for (int j = 0; j <= 2; j++)
+		for (j = 0; j < 3; j++)
 		{
-			if (grid1[i][j] > 3)
-			{
-				if (i > 0)
-					grid2[i - 1][j] += 1;
-				if (j > 0)
-					grid2[i][j - 1] += 1;
-				if (i < 2)
-					grid2[i + 1][j] += 1;
-				if (j < 2)
-					grid2[i][j + 1] += 1;
-				grid1[i][j] -= 4;
+			if (grid[i][j] > 3)
+			{   grid[i][j] -= 4;
+				if ((i + 1) >= 0 && (i + 1) < 3)
+					r_grid[i + 1][j] += 1;
+				if ((j + 1) >= 0 && (j + 1) < 3)
+					r_grid[i][j + 1] += 1;
+				if ((i - 1) >= 0 && (i - 1) < 3)
+					r_grid[i - 1][j] += 1;
+				if ((j - 1) >= 0 && (j - 1) < 3)
+					r_grid[i][j - 1] += 1;
 			}
 		}
 	}
-	add_sandpiles(grid1, grid2);
+	add_sandpiles(grid, r_grid);
 }
 
 /**
@@ -90,11 +90,11 @@ void check_piles(int grid1[3][3], int grid2[3][3])
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	add_sandpiles(grid1, grid2);
-	while (is_unstable(grid1))
+	while (is_unstable(grid1) == 0)
 	{
 		printf("=\n");
 		print_grid(grid1);
-		check_piles(grid1, grid2);
+		check_piles(grid1);
 	}
 
 }
