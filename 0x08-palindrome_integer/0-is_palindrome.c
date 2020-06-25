dc5db4981f30e4fe2cdb5a8c3b8960cfb2ab3978
Diego <777@holbertonschool.com>
2020-06-24 20:03:54 -0500

    pal

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - Checks if an int is a palindrome or not
+ * @n: Number given to be checked
+ *
+ * Return: 1 if @n is a palindrome, otherwise 0
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long rev = 0, rem = 0, num;
+
+	num = n;
+
+	while (n != 0)
+	{
+		rem = n % 10;
+		rev = rev * 10 + rem;
+		n /=10;
+	}
+
+	if (reve == num)
+		return (1);
+	return (0);
+}
